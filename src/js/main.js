cfff4ce89ad80ccd44ac5bf6c0feca9618058bea
Filipe Moreira <filimor@posteo.com.br>
2021-02-09 23:34:01 -0300

    Create the snake

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,25 @@ class Game {
     this.canvas = document.querySelector('.snake');
     this.context = this.canvas.getContext('2d');
     this.box = 32;
+    this.snake = [{
+      x: 8 * this.box,
+      y: 8 * this.box
+    }];
 
     this.createBackground();
+    this.createSnake();
   }
 
   createBackground() {
-  this.context.fillStyle = 'lightgreen';
-  this.context.fillRect(0, 0, 16 * this.box, 16 * this.box);
+    this.context.fillStyle = 'lightgreen';
+    this.context.fillRect(0, 0, 16 * this.box, 16 * this.box);
+  }
+
+  createSnake() {
+    this.snake.forEach((s) => {
+      this.context.fillStyle = 'green';
+      this.context.fillRect(s.x, s.y, this.box, this.box);
+    });
   }
 }
 
