1b23ca8676d08ee5ae1cdcd7a2856182fa027aa0
Filipe Moreira <filimor@posteo.com.br>
2021-02-10 00:29:21 -0300

    Implement snake moves

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,8 @@ class Game {
       x: 8 * this.box,
       y: 8 * this.box
     }];
-
-    this.createBackground();
-    this.createSnake();
+    this.direction = 'right';
+    setInterval(() => this.startGame(), 100);
   }
 
   createBackground() {
@@ -23,6 +22,63 @@ class Game {
       this.context.fillRect(s.x, s.y, this.box, this.box);
     });
   }
+
+  startGame() {
+    if (this.snake[0].x > 15 * this.box && this.direction === 'right') {
+      this.snake[0].x = 0;
+    } else if (this.snake[0].x < 0 && this.direction === 'left') {
+      this.snake[0].x = 16 * box;
+    } else if (this.snake[0].y > 15 * this.box && this.direction === 'down') {
+      this.snake[0].y = 0;
+    } else if (this.snake[0].y < 0 && this.direction === 'up') {
+      this.snake[0].y = 16 * box;
+    }
+
+    this.createBackground();
+    this.createSnake();
+
+    let snakeX = this.snake[0].x;
+    let snakeY = this.snake[0].y;
+
+    switch (this.direction) {
+      case 'right':
+       snakeX += this.box;
+        break;
+      case 'left':
+        snakeX -= this.box;
+        break;
+      case 'up':
+        snakeY -= this.box;
+        break;
+      case 'down':
+        snakeY += this.box;
+        break;
+      default:
+        break;
+    }
+
+    this.snake.pop();
+
+    this.snake.unshift({
+      x: snakeX,
+      y: snakeY
+    });
+  }
+}
+
+function update(event) {
+  event.preventDefault();
+
+  if (event.key === 'ArrowLeft' && game.direction != 'right') {
+    game.direction = 'left';
+  } else if (event.key === 'ArrowRight' && game.direction != 'left') {
+    game.direction = 'right';
+  } else if (event.key === 'ArrowDown' && game.direction != 'up') {
+    game.direction = 'down';
+  } else if (event.key === 'ArrowUp' && game.direction != 'down') {
+    game.direction = 'up';
+  }
 }
 
 const game = new Game();
+document.body.addEventListener('keydown', update);
